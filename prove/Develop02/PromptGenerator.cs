c0fd0a34b756ce5ca261177a59c4f569d431d308
Aleksandr_Shimanovich <realbreadinside@gmail.com>
2024-12-01 07:50:19 +0100

    public

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,7 +1,7 @@
 using System;
 public class PromptGenerator()
 {
-    private List<string> _prompts = new List<string>
+    public List<string> _prompts = new List<string>
     {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
@@ -10,7 +10,7 @@ public class PromptGenerator()
         "If I had one thing I could do over today, what would it be?"
     };
 
-    private Random _random = new Random();
+    public Random _random = new Random();
 
     public string GetRandomPrompt()
     {
