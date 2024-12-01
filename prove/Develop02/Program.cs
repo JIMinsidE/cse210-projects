9e8213ab31f4921d6b21190fff242aeaff21d869
Aleksandr_Shimanovich <realbreadinside@gmail.com>
2024-12-01 07:26:19 +0100

    print and input in pr...cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -10,6 +10,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("This is your daily journal. Please chose one of the next functions:\n 1. Write\n 2. Display\n 3. Load\n 4. Save\n 5. Quit");
+        string tinput = Console.ReadLine();
+        int input = int.Parse(tinput);
+
+        if (input == 1){
+
+        }
+        else if(input == 2){
+
+        }
+        else if(input == 3){
+            
+        }
+        else if(input == 4){
+            
+        }
+        else if(input == 5){
+            
+        }
     }
 }
