81721e5475afff4eca1c2e5662a0892ca4aea110
Aleksandr_Shimanovich <realbreadinside@gmail.com>
2024-12-01 06:58:29 +0100

    first changes

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,18 @@
+public class Journal()
+{
+    List<Entry> _entries = new List<Entry>;
+
+    public void AddEntry(newEntry: Entry)
+    {
+
+    }
+public void DisplayAll()
+{
+
+}
+public void SaveToFile(string filename)
+{
+
+}
+public void LoadFromFile(string filename){
+}
--- w01-prove-developer—c#-programming/Program.cs
+++ w01-prove-developer—c#-programming/Program.cs
@@ -16,66 +16,21 @@ class HelloWorld {
     Console.WriteLine($"Your name is {fname}, {fname} {lname}");
     
     /*
-    Task 3. 
+    Task 2. 
     */
     Console.WriteLine("Input your number");
     string tnumber = Console.ReadLine();
-
     int number = int.Parse(tnumber);
     int guess = 0;
-    int count = 0;
-
     while (guess != number)
     {
       Console.WriteLine("Guess the number");
-
       string tguess = Console.ReadLine();
       guess = int.Parse(tguess);
-      
       if (guess > number){
-        Console.WriteLine("Lower");
+        
       }
-
-      if (guess < number){
-        Console.WriteLine("Higher");
-      }
-    count += 1; 
     }
-    Console.WriteLine("You guessed it!");
-    Console.WriteLine($"Your number is: {number}");
-
-    Console.WriteLine("do you want to play more? y/n");
-
-    string answer = Console.ReadLine();
-    ////
-    if (answer == "Y" | answer == "y"){
-      
-      Console.WriteLine("Input your number");
-    tnumber = Console.ReadLine();
-
-    number = int.Parse(tnumber);
-    guess = 0;
-    count = 0;
-    while (guess != number)
-    {
-      Console.WriteLine("Guess the number");
-
-      string tguess = Console.ReadLine();
-      guess = int.Parse(tguess);
-      
-      if (guess > number){
-        Console.WriteLine("Lower");
-      }
-
-      if (guess < number){
-        Console.WriteLine("Higher");
-      }
-    count += 1; 
-    }
-////
-    }
-    if (answer == "N" | answer == "n"){
-
-    }
+    Console.WriteLine(number);
   }
 }
