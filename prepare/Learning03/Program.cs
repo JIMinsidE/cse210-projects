9f67946027c5c4a29d5b9ee596e1a983ed86779c
Aleksandr_Shimanovich <realbreadinside@gmail.com>
2024-12-04 06:30:50 +0100

    there was a small catch

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -12,11 +12,11 @@ class Program
         Console.WriteLine(frac2.GetFractionString());
         Console.WriteLine(frac2.GetDecimalValue());
 
-        Fraction frac3 = new Fraction(3/4);
+        Fraction frac3 = new Fraction(3,4);
         Console.WriteLine(frac3.GetFractionString());
         Console.WriteLine(frac3.GetDecimalValue());
 
-        Fraction frac4 = new Fraction(1/3);
+        Fraction frac4 = new Fraction(1,3);
         Console.WriteLine(frac4.GetFractionString());
         Console.WriteLine(frac4.GetDecimalValue());
     }
