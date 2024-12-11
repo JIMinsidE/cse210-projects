75d6c080e18a633d8677a30682ea68e95047ec57
Aleksandr_Shimanovich <realbreadinside@gmail.com>
2024-12-11 07:37:36 +0100

    Activity

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.WriteLine("Menu Options");
+        string input = Console.ReadLine();
+        if (input == "1")
+        {
+            Console.Write("S");
+        }
+
+
+        public class BreathingActivity
+            {
+                public string x;
+            }
+        public class ReflectionActivity
+            {
+                
+            }
+
+            public class ListingActivity
+            {
+                
+            }
     }
 }
